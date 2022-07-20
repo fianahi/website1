00d32e6ae27dc534f1b1f05ce17bf9449736bddd
fianahi <109094199+fianahi@users.noreply.github.com>
2022-07-20 12:01:28 -0500

    java1

---
+++ javascript.js
@@ -0,0 +1,117 @@
+// single line comment 
+
+/*
+
+this 
+is
+a multi-line
+comment
+
+*/ 
+
+//declar
+
+var myName;
+
+// numbers, letters, $ -
+// not spaces and can't start with a number
+
+var my_Var
+var myVar_;
+
+
+var a;
+a = 47;
+//console.log(a);
+//console.log("this is a");
+
+var b;
+b = a;
+console.log("b is equal to: " + b);
+
+
+// assignment operator
+var c = 0;
+
+//string
+var myFirstName = "Sofia";
+var myLastName= "Olivares Garcia";
+var num = "12";
+
+var number = "code";
+
+var x = 1;
+var y = 5;
+var z = "I am a";
+
+x = x + 1;
+y = y + 5;
+z = z + "Coder";
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+
+// latest version will override first
+var camper = "Sheetal";
+var camper = "Asher";
+console.log(camper);
+
+// let doesn't let you override... once it has been defined once it cannot be defined to another value 
+let student = "Sheetal";
+//let student = "Asher";
+
+console.log(student);
+
+// in const, no one can override. Helpful when you for sure don't want the value of seomthing to change 
+// usually written in caps to let you know it's a const
+const FAV_PET = "Cats";
+//FAV_PET = "Dogs";
+
+const SUM = 10 + 10;
+const DIFFERENCE = 45 -20;
+const QUOTIENT = 66 / 33;
+const PRODUCT = 8 * 10;
+
+console-log(SUM);
+console-log(DIFFERENCE);
+console-log(QUOTIENT);
+console-log(PRODUCT);
+
+var sum = 15+1;
+console-localStorage(sum)
+
+console.log(sum);
+
+let sum1 = 14 + 1;
+
+
+
+let newVar = 27;
+newvar =  newVar  + 1;
+newVar++;
+
+console.log(newVar);
+
+newVar = newVar - 1
+newVar--;
+
+let v = 1;
+v += 5;
+console.log("this is v: " + v);
+
+let s = 10;
+s -= 5;
+console.log("this is s: " +s);
+
+let m = 5;
+m *= 2;
+console.log("this is m: " + m);
+
+let d = 10
+d /= 2;
+console.log ("this is d: " + d)
+
+
+
